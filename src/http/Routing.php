abfd8f547417ade152cb778ea67152e0db491c9c
Hilkiah Makemo <hilmak01@gmail.com>
2018-02-05 07:57:49 -0700

    odified Routing class

--- src/http/Routing.php
+++ src/http/Routing.php
@@ -4,6 +4,7 @@ namespace TruMVC\Http;
 
 use FastRoute\Route;
 use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher as dispatcher;
 
 class Routing extends RouteCollector
 {
@@ -61,9 +62,57 @@ class Routing extends RouteCollector
         // }
     }
 
-    public function dispatch(Closure $router)
+    public function getHandler($handler)
     {
-        dump($this->routes);
-        return \FastRoute\simpleDispatcher($router);         
+        return function() use($handler){
+            if($handler instanceof \Closure) return $handler();
+            if(is_array($handler)) return implode("::", $handler);
+        };
+    }
+
+    public function dispatch()
+    {
+        // Fetch method and URI from somewhere
+        $httpMethod = $_SERVER['REQUEST_METHOD'];
+        $uri = $_SERVER['REQUEST_URI'];
+
+        // Strip query string (?foo=bar) and decode URI
+        if (false !== $pos = strpos($uri, '?')) {
+            $uri = substr($uri, 0, $pos);
+        }
+        $uri = rawurldecode($uri);
+
+        $this->router = dispatcher(function() {
+
+            foreach($this->routes as $name => $routes){
+                foreach($routes as $url => $route){
+                    $handler = $this->getHandler($route['class']);
+                    if($handler == null || $handler() == null) continue;
+                    $method = strtoupper($route['method']);
+                    if($method == 'ANY'){
+                        $method = ['GET','POST', 'PUT','DELETE', 'PATCH'];
+                    }                    
+                    $this->addRoute($method, $url, $handler);
+                }
+            }
+            die();
+
+        })->dispatch($httpMethod, $uri);
+
+        // $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+        switch ($this->router[0]) {
+        case \FastRoute\Dispatcher::NOT_FOUND:
+            // ... 404 Not Found
+            break;
+        case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            $allowedMethods = $this->router[1];
+            // ... 405 Method Not Allowed
+            break;
+        case \FastRoute\Dispatcher::FOUND:
+            $handler = $this->router[1];
+            $vars = $this->router[2];
+            dump($handler, $vars);
+            break;
+        }        
     }
 }
